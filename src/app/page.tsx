39c18573e391653f94c4954b970a9f121a39fcfb
ahmadzia-2024 <ahmadziaseyar2024@gmail.com>
2024-12-18 12:05:01 +0430

    Adding the Service, Photo To The Main Page and partial Resume Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import Photo from "@/components/Photo";
 import Social from "@/components/Social";
+import Stats from "@/components/Stats";
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
@@ -7,7 +9,7 @@ const Home = () => {
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
               Hello I'm <br />{" "}
@@ -37,8 +39,11 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div>Photo</div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
         </div>
+        <Stats />
       </div>
     </section>
   );
--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,5 +1,187 @@
+"use client";
+import { FaHtml5, FaCss3, FaReact } from "react-icons/fa";
+import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+import { motion } from "framer-motion";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+const about = {
+  title: "About me",
+  description:
+    "lorem ipsum dolor sit amet consectetur adip eu fugiat null et null et description eu fugiat null et description eu fugiat",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Ahmad Zia Seyar",
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "+93 77 161 3037",
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "1+ years",
+    },
+    {
+      fieldName: "Skype",
+      fieldValue: "ahmad-2024",
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "Afghan",
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "ahmadziaseyar2024@gmail.com",
+    },
+    {
+      fieldName: "Freelance",
+      fieldValue: "Available",
+    },
+    {
+      fieldName: "Languages",
+      fieldValue: "English, Pashto",
+    },
+  ],
+};
+
+const exprience = {
+  icon: "",
+  title: "My Experenence",
+  description:
+    "lorem ipsum dolor sit amet consectetur adip eu fugiat null et null et description eu fugiat null et description eu fugiat",
+  items: [
+    {
+      company: "Tech Solutions Inc",
+      postion: "Full Stack Developer",
+      duraton: "2022 - Present",
+    },
+    {
+      company: "Tech Solutions Inc",
+      postion: "Full Stack Developer",
+      duraton: "2022 - Present",
+    },
+    {
+      company: "Tech Solutions Inc",
+      postion: "Full Stack Developer",
+      duraton: "2022 - Present",
+    },
+    {
+      company: "Tech Solutions Inc",
+      postion: "Full Stack Developer",
+      duraton: "2022 - Present",
+    },
+    {
+      company: "Tech Solutions Inc",
+      postion: "Full Stack Developer",
+      duraton: "2022 - Present",
+    },
+    {
+      company: "Tech Solutions Inc",
+      postion: "Full Stack Developer",
+      duraton: "2022 - Present",
+    },
+    {
+      company: "Tech Solutions Inc",
+      postion: "Full Stack Developer",
+      duraton: "2022 - Present",
+    },
+  ],
+};
+const education = {
+  icon: "",
+  title: "My Edutcations",
+  description:
+    "lorem ipsum dolor sit amet consectetur adip eu fugiat null et null et description eu fugiat null et description eu fugiat",
+  items: [
+    {
+      institute: "Tech Solutions Inc",
+      degree: "Full Stack Developer",
+      duraton: "2022 - Present",
+    },
+    {
+      institute: "Tech Solutions Inc",
+      degree: "Full Stack Developer",
+      duraton: "2022 - Present",
+    },
+    {
+      institute: "Tech Solutions Inc",
+      degree: "Full Stack Developer",
+      duraton: "2022 - Present",
+    },
+    {
+      institute: "Tech Solutions Inc",
+      degree: "Full Stack Developer",
+      duraton: "2022 - Present",
+    },
+    {
+      institute: "Tech Solutions Inc",
+      degree: "Full Stack Developer",
+      duraton: "2022 - Present",
+    },
+    {
+      institute: "Tech Solutions Inc",
+      degree: "Full Stack Developer",
+      duraton: "2022 - Present",
+    },
+    {
+      institute: "Tech Solutions Inc",
+      degree: "Full Stack Developer",
+      duraton: "2022 - Present",
+    },
+  ],
+};
+const skills = {
+  title: "My Skills",
+  description:
+    "lorem ipsum dolor sit amet consectetur adip eu fugiat null et null et description eu fugiat null et description eu fugiat",
+  skillList: [
+    {
+      name: "HTML5",
+      icon: <FaHtml5 size="20" />,
+    },
+    {
+      name: "CSS3",
+      icon: <FaCss3 size="20" />,
+    },
+    {
+      name: "React",
+      icon: <FaReact size="20" />,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: <SiTailwindcss size="20" />,
+    },
+    {
+      name: "Next.js",
+      icon: <SiNextdotjs size="20" />,
+    },
+  ],
+};
 const Resume = () => {
-  return <div>Resume</div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="conatiner mx-auto">
+        <Tabs
+          defaultValue="experience"
+          className="flex flex-col xl:flex-row gap-[60px]"
+        >
+          <TabsList>
+            <TabsTrigger value="">Experience</TabsTrigger>
+            <TabsTrigger value="">Education</TabsTrigger>
+            <TabsTrigger value="">Skills</TabsTrigger>
+            <TabsTrigger value="">AboutMe</TabsTrigger>
+          </TabsList>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Resume;
